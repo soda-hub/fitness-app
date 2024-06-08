0fb418f2601f7990372fad09a49c17024cd0dabc
soda-hub <sotarockyz@gmail.com>
2024-06-08 16:43:56 +0900

    k

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 
 import { Route, Routes } from "react-router-dom";
-import { CssVarsProvider, extendTheme } from '@mui/joy/styles';
+import { CssVarsProvider } from '@mui/joy/styles';
 import CssBaseline from '@mui/joy/CssBaseline';
 
 
 import Home from './routes/home/Home'
 import Training from './routes/training/Training'
-
-const theme = extendTheme({
-
-})
 
 export default function App() {
   return (
